143e914ea87697dacfd360d7aae5c4e0209e6826
Tallys Martins <tallysmartins@gmail.com>
2019-08-28 14:05:34 -0300

    Draw two triangles with glDrawArrays
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -61,27 +61,24 @@ int main()
     float vertices[] = {
       -0.5f, -0.5f, 0.0f,
       0.5f, -0.5f, 0.0f,
-      0.5f,  0.5f, 0.0f,
+      -0.5f,  0.5f, 0.0f,
+      0.5f,  -0.5f, 0.0f,
+      0.5f, 0.5f, 0.0f,
       -0.5f, 0.5f, 0.0f
     };
 
-    unsigned int indices[] = {0, 1, 3, 1, 2, 3};
 
     // Allocate a buffer in the GPU and save its id in VBO
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(0);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
@@ -166,7 +163,10 @@ int main()
       glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
       // Draw elements from Element Buffer Object (use indices to avoid duplicated data)
-      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDrawArrays(GL_TRIANGLES, 3, 3);
 
       // glBindVertexArray(0); // no need to unbind it every time
 
